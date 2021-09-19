6c2f60aa7f51574f469cf62314b0dafd98cdf4cb
hdh <ghdengus12@naver.com>
2021-09-19 17:41:05 +0900

    [tespd] popup 생성 / popup 스크립트 작업

--- js/script.js
+++ js/script.js
@@ -37,7 +37,22 @@ $(function(){
     //     }
     
     // })
-    
 
+    //버튼 누를때마다 리스트 삭제 , 모달 닫기
+    // popup_slide > swiper-wrapper > swiper-slide 
+    $('.popup_close').click(function(){
+        $('.popup_slide').remove('li.swiper-slide'); 
+        $('.overlay').css('display','none');
+        $('.popup').css('display','none');
+        $('html, body').css('overflow','visible')
+    });
+
+    //modal 띄우기
+    $('.section dd .thumb').click(function(){
+        $('.overlay').css('display','block');
+        $('.popup').css('display','block');
+        $('html, body').css('overflow','hidden')
+        return false;
+    })
 
 })
