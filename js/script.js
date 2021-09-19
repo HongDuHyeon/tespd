241e73374e39a69b883b76032777990d5c67e1b4
hdh <ghdengus12@naver.com>
2021-09-19 17:05:00 +0900

    .DS_Store removed

--- js/script.js
+++ js/script.js
@@ -16,6 +16,28 @@ $(function(){
     });
 
 
+    // $('.wrap > div').click(function(){
+    //     var clickInx= $(this).index()+1; // 각 영역 위치
+    //     var arr = ['1','2','3'];
+    //     var img = $('img');
+    //     var n = 0;
+    
+    //     while (n < 10) {
+    //         n++;
+    
+    //         //이미지 객체생성
+    //         const image = new Image();
+    //         image.src = './img/slide'+arr[clickInx-1]+'-'+n+'.jpg';
+    
+    //         //image 태그 생성
+    //         $(this).find('ul').append(
+    //                 '<li class="swiper-slide"><img src="./img/slide'+clickInx+'-'+n+'.jpg" onerror="this.remove ? this.parentElement.remove() : this.removeNode()">'+'</li>'
+    //                 );
+    
+    //     }
+    
+    // })
+    
 
 
 })
